6854cd91872b98bec3a8724df065b1b352185269
v1ztep <v1ztep@gmail.com>
2021-11-27 04:07:22 +0300

    Создал app places

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from where_to_go import views
+from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
