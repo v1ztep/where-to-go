567439ebe903f539c35820aeb7575e2260e9a56f
v1ztep <v1ztep@gmail.com>
2021-11-28 19:57:32 +0300

    Настроил MEDIA

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from places import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('tinymce/', include('tinymce.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
