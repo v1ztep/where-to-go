31dd70565817b2c620917fa2c97a6df608caa38a
v1ztep <v1ztep@gmail.com>
2021-11-28 17:46:32 +0300

    Добавил модель Image

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+
+from .models import Image
 from .models import Place
 
 admin.site.register(Place)
+admin.site.register(Image)
--- places/models.py
+++ places/models.py
@@ -3,12 +3,23 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Заголовок")
-    description_short = models.TextField(verbose_name="Краткое описание")
-    description_long = HTMLField(verbose_name="Полное описание")
-    lng = models.FloatField(verbose_name="Долгота")
-    lat = models.FloatField(verbose_name="Широта")
-
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description_short = models.TextField(verbose_name='Краткое описание')
+    description_long = HTMLField(verbose_name='Полное описание')
+    lng = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта')
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    picture = models.ImageField(verbose_name='Картинка', upload_to='places/')
+    place = models.ForeignKey(
+        Place, verbose_name='Место картинки',
+        related_name='images', on_delete=models.CASCADE
+    )
+    number_of_image = models.PositiveIntegerField(verbose_name='Номер картинки')
+    
+    def __str__(self):
+        return f'{self.number_of_image} {self.place}'
