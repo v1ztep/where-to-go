d9bb8deb4b0f589613e772d4ff9a017f219b1697
v1ztep <v1ztep@gmail.com>
2021-11-30 01:20:17 +0300

    Выгрузил GeoJSON из HTML в views

--- places/views.py
+++ places/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from .models import Place
+
 
 def index(request):
-    return render(request, 'index.html')
+    places = Place.objects.all()
+    details_of_places = []
+
+    for place in places:
+        detail_place = {
+            'type': 'FeatureCollection',
+            'features': [{
+                'type': 'Feature',
+                'geometry': {
+                  'type': 'Point',
+                  'coordinates': [place.lng, place.lat]
+                },
+                'properties': {
+                  'title': place.title,
+                  'placeId': place.id,
+                  'detailsUrl': '../static/places/moscow_legends.json'
+                }
+            }]
+        }
+        details_of_places.append(detail_place)
+
+    context = {'value': details_of_places}
+    return render(request, 'index.html', context=context)
