50eeb91c394c92e68ae78b497c6b8fe429007017
v1ztep <v1ztep@gmail.com>
2021-11-30 09:08:56 +0300

    Упростил добавление фоток (добавил инлайн в локации)

--- places/admin.py
+++ places/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import Image
 from .models import Place
 
-admin.site.register(Place)
-admin.site.register(Image)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline
+    ]
