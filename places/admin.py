a309ecb2c8782007c07f3e83f128c41e3e5cc007
v1ztep <v1ztep@gmail.com>
2021-11-30 09:41:14 +0300

    Добавил превью фоток в админке

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Image
 from .models import Place
@@ -10,6 +11,17 @@ class ImageAdmin(admin.ModelAdmin):
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['preview_image']
+    fields = ["picture", "place", "preview_image", "number_of_image"]
+
+    def preview_image(self, obj):
+        return format_html(
+            '<img src="{url}" width="{width}" height={height} />'.format(
+                url=obj.picture.url,
+                width='auto',
+                height='200px',
+            )
+        )
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
