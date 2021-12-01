1f4ca4f4f1e45922265db25deb801887367bb7bc
v1ztep <v1ztep@gmail.com>
2021-12-01 06:39:30 +0300

    Подключил WYSIWYG-редактор

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 from places import views
 
@@ -23,5 +24,6 @@ from places import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('places/<int:place_id>/', views.post, name='place')
+    path('places/<int:place_id>/', views.post, name='place'),
+    path('tinymce/', include('tinymce.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
