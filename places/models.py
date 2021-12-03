556b0936c6e191c53eaef8decff0a21bfe97d61f
v1ztep <v1ztep@gmail.com>
2021-12-03 21:45:46 +0300

    Добавил в админке blank=True описанию

--- places/models.py
+++ places/models.py
@@ -4,8 +4,10 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Заголовок')
-    description_short = models.TextField(verbose_name='Краткое описание')
-    description_long = HTMLField(verbose_name='Полное описание')
+    description_short = models.TextField(
+        verbose_name='Краткое описание', blank=True
+    )
+    description_long = HTMLField(verbose_name='Полное описание', blank=True)
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
 
